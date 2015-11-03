f12717d130367af844ed540f52524e579c8c3d5b
jasonlemay <jason.lemay@arcbees.com>
2015-11-03 14:36:26 -0500

    Styling Chosen example

--- src/main/java/com/arcbees/chosenstyling/client/application/home/HomeView.java
+++ src/main/java/com/arcbees/chosenstyling/client/application/home/HomeView.java
@@ -3,8 +3,12 @@ package com.arcbees.chosenstyling.client.application.home;
 import javax.inject.Inject;
 
 import com.arcbees.chosen.client.ChosenOptions;
+import com.arcbees.chosen.client.gwt.ChosenListBox;
 import com.arcbees.chosen.client.gwt.MultipleChosenValueListBox;
+import com.arcbees.chosenstyling.client.resources.ChosenDarkResources;
+import com.arcbees.chosenstyling.client.resources.ChosenUnicornResources;
 import com.google.common.collect.Lists;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.text.shared.AbstractRenderer;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
@@ -41,9 +45,20 @@ public class HomeView extends ViewImpl implements HomePresenter.MyView {
         }
     }
 
+    private static final ChosenDarkResources CHOSEN_RESOURCES_DARK = GWT.create(ChosenDarkResources.class);
+    private static final ChosenUnicornResources CHOSEN_RESOURCES_UNICORN = GWT.create(ChosenUnicornResources.class);
+
     //Don’t forget that the component has to be provided
     @UiField(provided = true)
     MultipleChosenValueListBox<Choices> multipleValues;
+    @UiField(provided = true)
+    MultipleChosenValueListBox<Choices> multipleValuesDark;
+    @UiField(provided = true)
+    MultipleChosenValueListBox<Choices> multipleValuesUnicorn;
+    @UiField(provided = true)
+    ChosenListBox singleChosenDark;
+    @UiField(provided = true)
+    ChosenListBox singleChosenUnicorn;
 
     @Inject
     HomeView(Binder uiBinder) {
@@ -54,10 +69,30 @@ public class HomeView extends ViewImpl implements HomePresenter.MyView {
 
         multipleValues = new MultipleChosenValueListBox<>(new ChoiceRenderer(), chosenOptions);
 
+        ChosenOptions chosenOptionsDark = new ChosenOptions();
+        chosenOptionsDark.setPlaceholderText("Choose your favorite character");
+        chosenOptionsDark.setResources(CHOSEN_RESOURCES_DARK);
+
+        singleChosenDark = new ChosenListBox(chosenOptionsDark);
+        multipleValuesDark = new MultipleChosenValueListBox<>(new ChoiceRenderer(), chosenOptionsDark);
+
+        ChosenOptions chosenOptionsUnicorn = new ChosenOptions();
+        chosenOptionsUnicorn.setPlaceholderText("Choose your favorite character");
+        chosenOptionsUnicorn.setResources(CHOSEN_RESOURCES_UNICORN);
+
+        singleChosenUnicorn = new ChosenListBox(chosenOptionsUnicorn);
+        multipleValuesUnicorn = new MultipleChosenValueListBox<>(new ChoiceRenderer(), chosenOptionsUnicorn);
+
         initWidget(uiBinder.createAndBindUi(this));
 
         //Acceptable and selected values are set after createAndBindUi
         multipleValues.setAcceptableValues(Lists.newArrayList(Choices.values()));
         multipleValues.setValue(Lists.newArrayList(Choices.VADER, Choices.LEIA));
+
+        multipleValuesDark.setAcceptableValues(Lists.newArrayList(Choices.values()));
+        multipleValuesDark.setValue(Lists.newArrayList(Choices.VADER, Choices.LEIA));
+
+        multipleValuesUnicorn.setAcceptableValues(Lists.newArrayList(Choices.values()));
+        multipleValuesUnicorn.setValue(Lists.newArrayList(Choices.VADER, Choices.LEIA));
     }
 }
--- src/main/java/com/arcbees/chosenstyling/client/resources/ResourceLoader.java
+++ src/main/java/com/arcbees/chosenstyling/client/resources/ResourceLoader.java
@@ -4,8 +4,12 @@ import javax.inject.Inject;
 
 public class ResourceLoader {
     @Inject
-    ResourceLoader(AppResources appResources) {
+    ResourceLoader(AppResources appResources,
+            ChosenDarkResources chosenResources,
+            ChosenUnicornResources chosenUnicornResources) {
         appResources.normalize().ensureInjected();
         appResources.style().ensureInjected();
+        chosenResources.css().ensureInjected();
+        chosenUnicornResources.css().ensureInjected();
     }
 }
